c3c84fdb8d83935acc0be5ea85021734b057c6f2
Steve Bishop <xipooo@gmail.com>
2021-06-17 10:53:38 -0700

    Updates some AC's for BreadDiscount

--- app/src/test/java/ShoppingCart/BreadDiscountTests.java
+++ app/src/test/java/ShoppingCart/BreadDiscountTests.java
@@ -2,19 +2,34 @@ package ShoppingCart;
 
 // As Carrie the customer I want to receive discounts on purchases of bread so that I pay less money
 public class BreadDiscountTests {
-    // GIVEN the customer places 2 tins of soup and one loaf of bread in the basket, and the date of purchase is between yesterday and 7 days after yesterday
-    // WHEN the price of the items is totaled 
+
+    // GIVEN the customer places 2 tins of soup and one loaf of bread in the basket,
+    // and the date of purchase is between yesterday and 7 days after yesterday
+    // WHEN the price of the items is totaled
     // THEN the price of the bread should be half the normal price
-    
-    // GIVEN the customer places 1 tins of soup and one load of bread in the basket and the date of purchase is between yesterday and 7 days after yesterday
+
+    // GIVEN the customer places 2 tins of soup and one loaf of bread in the basket,
+    // and the date of purchase is yesterday
+    // WHEN the price of the items is totaled
+    // THEN the price of the bread should be half the normal price
+
+    // GIVEN the customer places 2 tins of soup and one loaf of bread in the basket,
+    // and the date of purchase is 7 days from yesterday
+    // WHEN the price of the items is totaled
+    // THEN the price of the bread should be half the normal price
+
+    // GIVEN the customer places 1 tins of soup and one load of bread in the basket
+    // and the date of purchase is between yesterday and 7 days after yesterday
     // WHEN the price of the items is totaled
     // THEN the price of the bread should be the normal price
-    
-    // GIVEN the customer places 6 tins of soup and 3 loafs of bread in the basket, and the date of purchase is between yesterday and 7 days after yesterday
+
+    // GIVEN the customer places 6 tins of soup and 3 loafs of bread in the basket,
+    // and the date of purchase is between yesterday and 7 days after yesterday
     // WHEN the prices of the items is totaled
     // THEN the price of each loaf of bread should be half the normal price
-    
-    // GIVEN the customer places 2 tins of soup and one loaf of bread in the basket, and the date of purchase is 8 days from today
-    // WHEN the price of the items is totaled 
+
+    // GIVEN the customer places 2 tins of soup and one loaf of bread in the basket,
+    // and the date of purchase is 8 days from today
+    // WHEN the price of the items is totaled
     // THEN the price of the bread should be the normal price
 }
