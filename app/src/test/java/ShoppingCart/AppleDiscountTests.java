10845534cd61c9fb0a5d30638f1ca14ade6ba876
Steve Bishop <xipooo@gmail.com>
2021-06-17 08:06:25 -0700

    Adds initial user stories and AC's

---
+++ app/src/test/java/ShoppingCart/AppleDiscountTests.java
@@ -0,0 +1,17 @@
+package ShoppingCart;
+
+//As Carrie the customer I want to receive discounts on purchases of apples so that I pay less money
+
+public class AppleDiscountTests {  
+    // GIVEN the customer placed an apple in the basket, and the date of purchase is between 3 days from today and the end of next month
+    // WHEN the price of the items is totaled
+    // THEN the price of the apple should be 10% less than the normal price
+    
+    // GIVEN the customer placed 5 apples in the basket, and the date of purchase is between 3 days from today and the end of next month
+    // WHEN the price of the items is totaled
+    // THEN the total price of the apples should be 10% less than the normal total price of the apples
+    
+    // GIVEN the customer placed an apple in the basket, and the date of purchase is today
+    // WHEN the price of the items is totaled
+    // THEN the price of the apple should be the normal price
+}
---
+++ app/src/test/java/ShoppingCart/BasketTests.java
@@ -0,0 +1,21 @@
+package ShoppingCart;
+
+// As Carrie the customer I want to add items to my shopping basket So that I can pay for them all at once
+
+public class BasketTests {
+    // GIVEN the application is running and there are no items in the basket
+    // WHEN zero units of soup are added to the basket
+    // THEN the basket should contain no items
+    
+    // GIVEN the application is running and there are no items in the basket
+    // WHEN a single bread loaf is added to the basket
+    // THEN the basket should contain one item
+    
+    // GIVEN the application is running and there are 2 milk bottles in the basket
+    // WHEN 5 apples are added to the basket
+    // THEN the basket should contain 7 items
+    
+    // GIVEN the application is running and there are no items in the basket
+    // WHEN 5 apples and 3 soup tins are added to the basket
+    // THEN the basket should contain 8 items
+}
