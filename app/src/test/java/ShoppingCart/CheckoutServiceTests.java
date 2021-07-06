731485050faea4212dc127d3f9babaa42be43ede
Steve Bishop <xipooo@gmail.com>
2021-07-06 14:49:13 -0700

    Adds scenarios for CheckoutService

---
+++ app/src/test/java/ShoppingCart/CheckoutServiceTests.java
@@ -0,0 +1,18 @@
+package ShoppingCart;
+
+public class CheckoutServiceTests {
+    // Price a basket containing: 3 tins of soup and 2 loaves of bread, bought
+    // today,
+    // Expected total cost = 3.15;
+
+    // Price a basket containing: 6 apples and a bottle of milk, bought today,
+    // Expected total cost = 1.90;
+
+    // Price a basket containing: 6 apples and a bottle of milk, bought in 5 days
+    // time,
+    // Expected total cost = 1.84;
+    
+    // Price a basket containing: 3 apples, 2 tins of soup and a loaf of bread,
+    // bought in 5 days time,
+    // Expected total cost = 1.97.
+}
